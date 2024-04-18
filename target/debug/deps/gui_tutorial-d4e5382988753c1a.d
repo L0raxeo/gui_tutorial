333454b202d386246e128e2d31ef7cabe92bf5cd
WOPR <fuzedcable@gmail.com>
2024-04-17 23:01:58 -0400

    test commit

---
+++ target/debug/deps/gui_tutorial-d4e5382988753c1a.d
@@ -0,0 +1,5 @@
+D:\Users\socce\Desktop\rust_test\gui_tutorial\target\debug\deps\gui_tutorial-d4e5382988753c1a.rmeta: src\main.rs
+
+D:\Users\socce\Desktop\rust_test\gui_tutorial\target\debug\deps\gui_tutorial-d4e5382988753c1a.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/gui_tutorial-df32d815f2642c42.d
@@ -0,0 +1,5 @@
+D:\Users\socce\Desktop\rust_test\gui_tutorial\target\debug\deps\gui_tutorial-df32d815f2642c42.rmeta: src\main.rs
+
+D:\Users\socce\Desktop\rust_test\gui_tutorial\target\debug\deps\gui_tutorial-df32d815f2642c42.d: src\main.rs
+
+src\main.rs:
